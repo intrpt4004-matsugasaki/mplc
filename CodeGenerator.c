3ae58ca57bbee223578908457f3561178f45e325
intrpt4004 <111030717+intrpt4004-matsugasaki@users.noreply.github.com>
2023-01-23 03:33:29 +0900

    update

--- CodeGenerator.c
+++ CodeGenerator.c
@@ -13,6 +13,26 @@ extern char *get_stem(char *filename) {
 	return stem;
 }
 
+static int get_size(variable_t *v) {
+	if (v->type.kind == STANDARD) return 1;
+	return v->type.array.size;
+}
+
 extern void generate_code(char *filename, program_t program) {
-	printf("%s", filename);
+	FILE *fp = fopen(filename, "w");
+
+	// program block
+	for (variable_t *v = program.var; v != NULL; v = v->next) {
+		fprintf(fp, "$%s     DS     %d\n", v->name, get_size(v));
+	}
+
+	// procedure block
+	for (procedure_t *p = program.proc; p != NULL; p = p->next) {
+		for (variable_t *v = p->var; v != NULL; v = v->next) {
+			fprintf(fp, "$%s.%s     DS     %d\n", p->name, v->name, get_size(v));
+		}
+	}
+
+	fprintf(fp, "END\n");
+	fclose(fp);
 }
